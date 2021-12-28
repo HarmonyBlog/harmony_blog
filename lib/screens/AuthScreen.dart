a53c879df7a75f2a1ebc086f8b7aba4ec87d72bf
rahulrajsbkk <rahulrajsbkk@gmail.com>
2021-12-28 17:48:39 +0530

    Auth Screen Done

--- lib/screens/AuthScreen.dart
+++ lib/screens/AuthScreen.dart
@@ -6,7 +6,45 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFFF),
-    );
+        backgroundColor: Color(0xfff5ebff),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icons/logo.png',
+              ),
+              Text(
+                'Harmony Blog',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Communicate Your Thinkings Inside',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 100.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xff009999)),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 25)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)))),
+                  child: Text('Sign In With IITM Google Account'),
+                  onPressed: () => {},
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
